37425e1e9aee2d7ddb3ac2d6d1e3fe0c7c63aab6
Luke Taaffe <luke.taaffe@hotmail.com>
2019-04-06 16:00:22 +0100

    fix babel with jest

--- __tests__/testWorkoutGenerator.js
+++ __tests__/testWorkoutGenerator.js
@@ -1,4 +1,10 @@
+import * from '../src/sum';
+import test from 'jest';
+
 // The workout generator is prefilled with users from json
+test('adds 1 + 2 = 3', () = {
+  expect(sum(1,2)).toBe(3);
+});
 
 // The workout generator is prefilled with exercises from json
 
