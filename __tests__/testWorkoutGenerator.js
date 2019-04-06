a954dee5dcd2c29a33ad1e9b4f79df819b57ee43
Luke Taaffe <luke.taaffe@hotmail.com>
2019-04-06 16:00:39 +0100

    workout generator tests fleshing out

--- __tests__/testWorkoutGenerator.js
+++ __tests__/testWorkoutGenerator.js
@@ -1,11 +1,35 @@
-import * from '../src/sum';
-import test from 'jest';
-
 // The workout generator is prefilled with users from json
-test('adds 1 + 2 = 3', () = {
-  expect(sum(1,2)).toBe(3);
+test('The workout generator is pre-filled with users from the users json file', function() {
+  // create a workout generator object
+
+  // generate the workout
+
+  // assert the generated workout object contains the expected values
 });
 
 // The workout generator is prefilled with exercises from json
+test('The workout generator is pre-filled with users from the users json file', function() {
+  // create a workout generator object
 
-// The workout generator can generate a workout data structure
+  // generate the workout
+
+  // assert the generated workout object contains the expected values
+});
+
+test('The workout generator has the expected number of exercise sets for a user', function() {
+  // Create a workout generator object with 30 sets
+
+  // The object should have 30 sets
+});
+
+test('The workout generator has the expected workout start time', function() {
+  // Create a workout generator object
+
+  // The start workout time should be at the next 10 minute interval
+});
+
+test('The workout generator has the expected workout end time', function() {
+  // Create a workout generator object
+
+  // The workout end time should be the number of sets * set time, after the workout start time
+});
