bebd4e4a022df4c13205d27b8bdc7fe3ed7dc240
Luke Taaffe <luke.taaffe@hotmail.com>
2019-04-07 13:08:58 +0100

    Flesh out workout outputter syntax

--- __tests__/testWorkoutOutputter.js
+++ __tests__/testWorkoutOutputter.js
@@ -1,7 +1,19 @@
 // The workout outputter can print output
+test('The output is printed to console', () => {
+
+});
 
 // The workout outputter prints the expected number of lines for workout sets
+test('The output prints the expected number of sets', () => {
+
+});
 
 // The workout outputter outputs the expected workout start and end times
+test('The output prints the expected workout start and end time', () => {
+
+});
 
 // The workout outputter outputs the expected set start and end time
+test('The output prints the expected start and end set time', () => {
+
+});
