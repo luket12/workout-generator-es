8ce81a47ec5ac62103b4da8815387afec45ec1df
Luke Taaffe <luke.taaffe@hotmail.com>
2019-04-07 13:08:45 +0100

    ES6 anonymous function syntax

--- __tests__/testWorkoutGenerator.js
+++ __tests__/testWorkoutGenerator.js
@@ -1,5 +1,5 @@
 // The workout generator is prefilled with users from json
-test('The workout generator is pre-filled with users from the users json file', function() {
+test('The workout generator is pre-filled with users from the users json file', () => {
   // create a workout generator object
 
   // generate the workout
@@ -8,7 +8,7 @@ test('The workout generator is pre-filled with users from the users json file', 
 });
 
 // The workout generator is prefilled with exercises from json
-test('The workout generator is pre-filled with users from the users json file', function() {
+test('The workout generator is pre-filled with users from the users json file', () => {
   // create a workout generator object
 
   // generate the workout
@@ -16,19 +16,19 @@ test('The workout generator is pre-filled with users from the users json file', 
   // assert the generated workout object contains the expected values
 });
 
-test('The workout generator has the expected number of exercise sets for a user', function() {
+test('The workout generator has the expected number of exercise sets for a user', () => {
   // Create a workout generator object with 30 sets
 
   // The object should have 30 sets
 });
 
-test('The workout generator has the expected workout start time', function() {
+test('The workout generator has the expected workout start time', () => {
   // Create a workout generator object
 
   // The start workout time should be at the next 10 minute interval
 });
 
-test('The workout generator has the expected workout end time', function() {
+test('The workout generator has the expected workout end time', () => {
   // Create a workout generator object
 
   // The workout end time should be the number of sets * set time, after the workout start time
