f4822282c3cbe8b1fa60bfe70b83efe315e7faf4
Luke Taaffe <ltaaffe@names.co.uk>
2019-04-23 17:01:51 +0100

    Use describe wrapper on all tests, add failing test for randomisation of workouts. ready for implementation

--- __tests__/testWorkoutGenerator.js
+++ __tests__/testWorkoutGenerator.js
@@ -13,58 +13,99 @@ beforeAll(() => {
   };
 });
 
-test('The workout generator set the correct set total and set times', () => {
-  // Create a workout generator object
-  const workoutGenerator = new WorkoutGenerator(30, 60);
+describe('The workout generator', () => {
+  let workoutSets = 30;
+  let setTime = 60;
+  const workoutGenerator = new WorkoutGenerator(workoutSets, setTime);
 
-  expect(workoutGenerator.getWorkoutSetTime()).toEqual(60);
-  expect(workoutGenerator.getWorkoutSets()).toEqual(30);
-});
 
-test('The workout generator has the expected workout start & end times', () => {
+  test('can set the correct set total and set times', () => {
+    expect(workoutGenerator.getWorkoutSetTime()).toEqual(60);
+    expect(workoutGenerator.getWorkoutSets()).toEqual(30);
+  });
 
-  // Create a workout generator object
-  const workoutGenerator = new WorkoutGenerator(30, 60);
-
-  // The start workout time should be at the next 10 minute interval and end 30 minutes later
-  expect(workoutGenerator.startTime).toEqual(new Date(1546344600000));
-  expect(workoutGenerator.endTime).toEqual(new Date(1546346400000));
-});
+  test('can set the expected workout start & end times', () => {
+    // The start workout time should be at the next 10 minute interval and end 30 minutes later
+    expect(workoutGenerator.startTime).toEqual(new Date(1546344600000));
+    expect(workoutGenerator.endTime).toEqual(new Date(1546346400000));
+  });
 
 // Come dn
 
-test('The workout generator has the expected number of exercise sets for a user', () => {
-  let users = [
-    {
-      'name': 'Jon',
-      'type': 'beginner'
-    }
-  ];
+  test('can set the expected number of exercise sets for a user', () => {
+    let users = [
+      {
+        'name': 'Jon',
+        'type': 'beginner'
+      }
+    ];
 
-  let exercises = [
-    {
-      "name": "Handstands"
-    }
-  ];
+    let exercises = [
+      {
+        "name": "Handstands"
+      }
+    ];
 
-  // create a workout generator object and generate the workout
-  const workoutGenerator = new WorkoutGenerator(30, 60);
+    workoutGenerator.generate(users, exercises);
+    let workouts = workoutGenerator.getWorkouts();
 
-  workoutGenerator.generate(users, exercises);
-  let workouts = workoutGenerator.getWorkouts();
+    // Check that exercise sets have been created
+    expect(workouts).toBeDefined();
+    expect(workouts.length).toBeGreaterThanOrEqual(1);
+    expect(workouts[0]).toBeInstanceOf(Workout);
 
-  // Check that exercise sets have been created
-  expect(workouts).toBeDefined();
-  expect(workouts.length).toBeGreaterThanOrEqual(1);
-  expect(workouts[0]).toBeInstanceOf(Workout);
+    // The generated workout should contain at least one user with a name and either beginner or advanced type
+    expect(workouts[0]).toMatchObject({
+      _user: 'Jon',
+    });
 
-  // The generated workout should contain at least one user with a name and either beginner or advanced type
-  expect(workouts[0]).toMatchObject({
-    _user: 'Jon',
-    _workoutSets: []
+    // Check there are 30 sets for the workout
+    expect(workouts[0].workoutSets.length).toEqual(30);
   });
 
-  // Check there are 30 sets for the workout
-  expect(workouts[0].workoutSets.length).toEqual(30);
+  test('can generate workout sets with random exercises', () => {
+    let users = [
+      {
+        'name': 'Jon',
+        'type': 'beginner'
+      }
+    ];
+
+    let exercises = [
+      {
+        "name": "Handstands"
+      },
+      {
+        "name": "Push-ups"
+      }
+    ];
+
+    // Generate two workouts
+    let workoutA = workoutGenerator.generate(users, exercises);
+    let workoutB = workoutGenerator.generate(users, exercises);
+
+    // Get a collection of indexes to compare in both workouts
+    let setCollection = getWorkoutSetComparisonIndexes(workoutSets);
+
+    // Find the number of matching sets from both workouts, if every index matches then it's not being randomised
+    let matches = setCollection.filter((item, index) => {
+        return workoutA[index] === workoutB[index];
+    });
+
+    expect(matches.length).toBeLessThan(setCollection.length);
+
+    // console.log("Set collection: ", setCollection.length);
+    // console.log("matches: " + matches.length);
+  });
 });
 
+function getRandomArbitrary(min, max) {
+  return Math.ceil(Math.random() * (max - min) + min);
+}
+
+function getWorkoutSetComparisonIndexes(workoutSets) {
+    return Array.apply(null, Array(getRandomArbitrary(3,6)))
+        .map(() => {
+            return getRandomArbitrary(1, workoutSets);
+        });
+}
