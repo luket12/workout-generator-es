c95efa19b7680800a300defb69b29940e445d57f
Luke Taaffe <ltaaffe@names.co.uk>
2019-04-24 11:42:39 +0100

    Exercise picking tests

---
+++ __tests__/testExerciseRandomiser.js
@@ -0,0 +1,41 @@
+describe('The exercise randomiser', () => {
+   test('can pick a random exercise', () => {
+      // Create some exercises
+
+      // Create a set of 4 workout sets
+
+      // Create another set of 4 workout sets
+
+      // Verify they dont all match
+   });
+
+   test('does not allow handstands twice in a row', () => {
+      // Create exercises
+
+      // Create some workout sets
+
+      // Find each workout set which has handstands
+
+      // Check the next workout set is definitely not a handstand also
+   });
+
+   test('does not allow cardio exercises twice in a row',() => {
+      // Create exercises
+
+      // Create some workout sets
+
+      // Find each workout set which has handstands
+
+      // Check the next workout set is definitely not cardio also
+   });
+
+   test('beginner have 4 breaks, any other user have 2', () => {
+      // Create some exercises
+
+      // Create a lot of workout sets
+
+      // Count the number of breaks are equal to 4 for beginners
+
+      // Count the number of breaks for other are 2
+   });
+});
